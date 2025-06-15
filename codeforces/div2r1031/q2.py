5d8dbc9db17d12a92f3c8e33c242aae86a191506
Advait Desai <advaitdesai0702@gmail.com>
2025-06-15 17:14:47 +0530

    as usual, post contest solve

--- codeforces/div2r1031/q2.py
+++ codeforces/div2r1031/q2.py
@@ -2,61 +2,29 @@ def solve():
     w, h, a, b = map(int, input().split())
     x1, y1, x2, y2 = map(int, input().split())
 
-    x_stat = True
+    x_gap = abs(x2 - x1)
+    y_gap = abs(y2 - y1)
 
-    # # check if overlap
-    # if max(x1, x2) <= min(x1 + b, x2 + b):
-    #     # gap in between
-    #     gap = max(x2 - (x1+b), x1 - (x2 + b))
-
-    #     if gap % a != 0:
-    #         x_no = False
-    #     else:
-    #         return "Yes"
-        
-    #     gap = max(y2 - (y1+a), y1 - (y2 + a))
-
-    #     if gap % b == 0:
-    #         return "Yes"
-    #     else:
-    #         if x_no == False:
-    #             return "No"
-    #         else:
-    #             return "Yes"
-
-    # elif max(y1, y2) <= min(y1 + a, y2 + a):
-    #     gap = max(x2 - (x1+b), x1 - (x2 + b))
-
-    #     if gap % a != 0:
-    #         x_no = False
-    #     else:
-    #         return "Yes"
-        
-    #     gap = max(y2 - (y1+a), y1 - (y2 + a))
-
-    #     if gap % b == 0:
-    #         return "Yes"
-    #     else:
-    #         if x_no == False:
-    #             return "No"
-    #         else:
-    #             return "Yes"
-            
-    # else:
-    #     return "Yes3"
-
-    # gap in between
-    gap = max(x2 - (x1+a), x1 - (x2 + a))
-
-    if gap % a == 0:
-        return "Yes"
+    if x_gap == 0:
+        if y_gap % b == 0:
+            return "Yes"
+        else:
+            return "No"
     
-    gap = max(y2 - (y1+b), y1 - (y2 + b))
+    if y_gap == 0:
+        if x_gap % a == 0:
+            return "Yes"
+        else:
+            return "No"
 
-    if gap % b == 0:
+
+    if x_gap % a == 0:
         return "Yes"
-    else:
-        return "No"
+
+    if y_gap % b == 0:
+        return "Yes"
+
+    return "No"
     
     
     
