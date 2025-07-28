22081508ea6c4efe68cd938ee9153255919933fa
Advait Desai <advaitdesai0702@gmail.com>
2025-07-28 15:13:51 +0530

    modifed template.py

--- template.py
+++ template.py
@@ -1,9 +1,11 @@
 def solve():
     n = int(input())
     a = list(map(int, input().split()))
-    a2 = map(int, input().split())
+    a3 = map(int, input().split())
+
 
 
 t = int(input())
+
 for _ in range(t):
     print(solve())
